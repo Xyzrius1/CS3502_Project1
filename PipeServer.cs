947120a60f5d9eca8c555e84f1e9d386200e2972
Xyzrius1 <aly.xyzrius@gmail.com>
2025-03-01 15:50:26 -0500

    Add files via upload

--- PipeServer.cs
+++ PipeServer.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
 
+class PipeServer
+{
+    static void HandleClient(object obj)
+    {
+        using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("mypipe"))
+        {
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Client connected!");
+
+            using (StreamWriter writer = new StreamWriter(pipeServer))
+            {
+                writer.AutoFlush = true;
+                writer.WriteLine("Hello from the server!");
+                Console.WriteLine("Message sent to client.");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Pipe Server is running...");
+
+        for (int i = 0; i < 3; i++)  // Accept up to 3 clients
+        {
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start();
+        }
+
+        Console.ReadLine(); // Keep server running
+    }
+}
