9eb3b6f78ce33192650d847c3fa8fce72673a79f
Xyzrius1 <aly.xyzrius@gmail.com>
2025-03-01 15:52:14 -0500

    Add files via upload

---
+++ PipeClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+class PipeClient
+{
+    static void Main()
+    {
+        Console.WriteLine("Connecting to Pipe Server...");
+
+        using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "mypipe", PipeDirection.In))
+        {
+            pipeClient.Connect();
+            Console.WriteLine("Connected to server!");
+
+            using (StreamReader reader = new StreamReader(pipeClient))
+            {
+                string message = reader.ReadLine();
+                Console.WriteLine("Received from server: " + message);
+            }
+        }
+
+        Console.WriteLine("Pipe Client has closed.");
+    }
+}
